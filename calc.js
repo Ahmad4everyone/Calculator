b07aabeab036ba723e8056c6df374ab88d6390f5
ahmad4everyone <ahmadmut24@gmail.com>
2023-11-15 21:58:12 -0800

    first commit

---
+++ calc.js
@@ -0,0 +1,10 @@
+
+
+function answer(x){
+    document.getElementById("ahmad").value += x
+
+}
+
+function Result(){
+    document.getElementById("ahmad").value = eval(document.getElementById("ahmad").value)
+}
